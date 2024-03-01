29236402c32eb2484602aeffe6419df65a4b9b8f
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-03-01 15:58:02 +0530

    update

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Example app listening on port 4000!');
+});
